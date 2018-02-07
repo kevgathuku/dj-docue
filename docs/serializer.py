0fd1146ba05d3e2fc830b70e4f18cbce92adbbe7
Kevin Ndung'u <kevgathuku@gmail.com>
2018-02-07 15:03:02 +0300

    Add Document serializer

---
+++ docs/serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Document
+
+
+class DocumentSerializer(serializers.ModelSerializer):
+    """Serializer to map the Model instance into JSON format."""
+
+    class Meta:
+        """Meta class to map serializer's fields with the model fields."""
+        model = Document
+        fields = '__all__'
+        read_only_fields = ('date_created', 'date_modified')
