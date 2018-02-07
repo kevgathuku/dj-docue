383340fa76bd44da1418e2db1ac7d050843a0c12
Kevin Ndung'u <kevgathuku@gmail.com>
2018-02-07 14:44:59 +0300

    Add docs model definition

--- docs/models.py
+++ docs/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from django.db import models  # noqa
 
-# Create your models here.
+from common.models import IndexedTimeStampedModel
+from users.models import User
+
+ALL = 'ALL'
+ONLY_ME = 'ONLY ME'
+STAFF = 'STAFF'
+ADMIN = 'ADMIN'
+
+DOC_ACCESS_CHOICES = (
+    (ALL, 'All'),
+    (ONLY_ME, 'Only Me'),
+    (STAFF, 'Staff'),
+    (ADMIN, 'Admin'),
+    )
+
+def get_sentinel_user():
+    """Return a generic user to assign deleted user accounts' docs to"""
+    return User.objects.get_or_create(username='deleted')[0]
+
+class Document(IndexedTimeStampedModel):
+    """Represents the Document model."""
+    title = models.CharField(max_length=255, blank=False)
+    content = models.CharField(max_length=255, blank=False)
+    owner = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
+    can_access = models.CharField(
+        max_length=20, blank=False, choices=DOC_ACCESS_CHOICES, default=ALL)
+
+    def __str__(self):
+        """Return a human readable representation of the Document instance."""
+        return "{}".format(self.title)
